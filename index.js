045447a6fb5945c2d32828f288af1d2f54ac725e
Le Viet Xuan <xuantryingbetter@gmail.com>
2025-02-19 05:24:46 +0700

    delete nodemodule from github

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const route = require("./routes/client/index.route");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
