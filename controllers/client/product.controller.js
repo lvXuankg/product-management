65e5a13895c5d88162b3e647ff4f0e07e15f753e
Le Viet Xuan <xuantryingbetter@gmail.com>
2025-02-19 05:07:47 +0700

    init the project

---
+++ controllers/client/product.controller.js
@@ -0,0 +1,9 @@
+module.exports.index = (req, res) => {
+    res.render("client/pages/products/index");
+}
+module.exports.create = (req, res) => {
+    res.render("client/pages/products/index");
+}
+module.exports.edit = (req, res) => {
+    res.render("client/pages/products/index");
+}
