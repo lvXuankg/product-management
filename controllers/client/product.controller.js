74647e98fedc3fb9bd1d65d2a33fdfc6590473f7
Le Viet Xuan <xuantryingbetter@gmail.com>
2025-02-19 05:49:32 +0700

    config pug folder

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,9 +1,15 @@
 module.exports.index = (req, res) => {
-    res.render("client/pages/products/index");
+    res.render("client/pages/products/index", {
+        pageTitle: "Danh sách sản phẩm"
+    });
 }
 module.exports.create = (req, res) => {
-    res.render("client/pages/products/index");
+    res.render("client/pages/products/index", {
+        pageTitle: "Tạo sản phẩm"
+    });
 }
 module.exports.edit = (req, res) => {
-    res.render("client/pages/products/index");
+    res.render("client/pages/products/index", {
+        pageTitle: "Sửa sản phẩm"
+    });
 }
