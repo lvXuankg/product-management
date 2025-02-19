af707a31aa52f19e7d9a7926c41b2d7f37a7938d
Le Viet Xuan <xuantryingbetter@gmail.com>
2025-02-19 16:17:52 +0700

    config

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ const express = require('express');
 require("dotenv").config();
 const database = require("./config/database");
 
+const systemConfig = require("./config/system");
+
 const route = require("./routes/client/index.route");
 const routeAdmin = require("./routes/admin/index.route");
+
 database.connect();
+
 const app = express();
 const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+// App Locals Variables 
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 app.use(express.static("public"));
 
