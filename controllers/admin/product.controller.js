9e9bdac97ba36770987b6220737e7d978cbce4ec
Le Viet Xuan <xuantryingbetter@gmail.com>
2025-02-20 14:59:27 +0700

    search function

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -30,12 +30,22 @@ module.exports.index = async (req, res) => {
         deleted: false
     }
     if(req.query.status) find.status = req.query.status;
+
+    let keyword = "";
+    if(req.query.keyword){
+        keyword = req.query.keyword;
+
+        const regex = new RegExp(keyword, "i");
+        find.title = regex;
+    }
+
     const products = await Product.find(find);
 
     // console.log(products);
     res.render("admin/pages/products/index", {
         pageTitle: "Trang danh sách sản phẩm",
         products: products,
-        filterStatus: filterStatus
+        filterStatus: filterStatus,
+        keyword: keyword
     });
 }
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -3,7 +3,7 @@
 const buttonsStatus = document.querySelectorAll("[button-status]");
 if(buttonsStatus.length > 0){
     let url = new URL(window.location.href);
-    console.log(url);
+    // console.log(url);
 
     buttonsStatus.forEach(button => {
         button.addEventListener("click", () => {
@@ -19,3 +19,21 @@ if(buttonsStatus.length > 0){
     });
 }
 // End Button Status 
+
+// Form Search 
+const formSearch = document.querySelector("#form-search");
+if(formSearch){
+    formSearch.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const keyword = e.target.elements.keyword.value;
+        let url = new URL(window.location.href);
+        if(keyword){
+            url.searchParams.set("keyword", keyword);
+        } else{
+            url.searchParams.delete("keyword");
+        }
+        window.location.href = url;
+    })
+}
+
+// End From Search 
