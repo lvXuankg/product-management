d65dcb904e701fd130469491fe1504b120fbd6e3
Le Viet Xuan <xuantryingbetter@gmail.com>
2025-02-22 22:44:03 +0700

    pagination function

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -16,13 +16,31 @@ module.exports.index = async (req, res) => {
     if(objectSearch.regex){
         find.title = objectSearch.regex;
     }
-    const products = await Product.find(find);
+
+    // Pagination
+    let objectPagination = {
+        currentPage: 1,
+        limitItems: 4
+    };
+    if(req.query.page &&!isNaN(req.query.page) && req.query.page > 0){
+        objectPagination.currentPage = parseInt(req.query.page);
+    }
+    objectPagination.skip = (objectPagination.currentPage-1)*objectPagination.limitItems;
+    // console.log(objectPagination.currentPage);
+
+    const countProducts = await Product.countDocuments(find);
+    const totalPage = Math.ceil(countProducts/objectPagination.limitItems);
+    objectPagination.totalPage = totalPage;
+    // End Pagination 
+
+    const products = await Product.find(find).limit(objectPagination.limitItems).skip(objectPagination.skip);
 
     // console.log(products);
     res.render("admin/pages/products/index", {
         pageTitle: "Trang danh sách sản phẩm",
         products: products,
         filterStatus: filterStatus,
-        keyword: objectSearch.keyword
+        keyword: objectSearch.keyword,
+        pagination: objectPagination
     });
 }
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -37,3 +37,20 @@ if(formSearch){
 }
 
 // End From Search 
+
+// Pagination
+const buttonsPagination = document.querySelectorAll("[button-pagination]");
+if(buttonsPagination){
+    let url = new URL(window.location.href);
+
+    buttonsPagination.forEach(button => {
+        button.addEventListener("click", () => {
+            const page = button.getAttribute("button-pagination");
+            console.log(page);
+
+            url.searchParams.set("page", page);
+            window.location.href = url.href;
+        });
+    });
+}
+// End Pagination 
