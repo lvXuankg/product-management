d0b13a6774446a13c32c72754e1e861539054ad1
Le Viet Xuan <xuantryingbetter@gmail.com>
2025-03-01 03:51:00 +0700

    validate

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -121,6 +121,7 @@ module.exports.create = (req, res) => {
 
 // [POST] /admin/products/create 
 module.exports.createPost = async (req, res) => {
+   
     // console.log(req.file);
     req.body.price = parseInt(req.body.price);
     req.body.stock = parseInt(req.body.stock);
@@ -132,7 +133,11 @@ module.exports.createPost = async (req, res) => {
     } else{
         req.body.position = parseInt(req.body.position);
     }
-    req.body.thumbnail = `/uploads/${req.file.filename}`;
+
+    if(req.file){
+        req.body.thumbnail = `/uploads/${req.file.filename}`;
+    }
+    
 
     const product = new Product(req.body);
     await product.save();
--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -5,6 +5,7 @@ const storageMulter = require("../../helpers/storageMulter");
 const upload = multer({storage: storageMulter()});
 const controller = require("../../controllers/admin/product.controller");
 
+const validate = require("../../validates/admin/product.validate");
 router.get("/", controller.index);
 
 router.patch("/change-status/:status/:id", controller.changeStatus);
@@ -18,6 +19,7 @@ router.get("/create", controller.create);
 router.post(
     "/create",
     upload.single('thumbnail'), 
+    validate.createPost,        //middleware validate
     controller.createPost
 );
 
