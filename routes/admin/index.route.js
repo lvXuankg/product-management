31465dd1fa85b69c89b0daa0e45c6c8491b33e87
Le Viet Xuan <xuantryingbetter@gmail.com>
2025-03-08 00:43:51 +0700

    display permission group

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -2,7 +2,7 @@ const dashboardRoutes = require("./dashboard.route");
 const product = require("./product.route");
 const productCategoryRoutes = require("./product-category.route");
 const systemConfig = require("../../config/system");
-
+const roleRoutes = require("./role.route");
 module.exports = (app) => {
     const PATH_ADMIN = systemConfig.prefixAdmin;
 
@@ -11,4 +11,6 @@ module.exports = (app) => {
     app.use(PATH_ADMIN + "/products", product );
 
     app.use(PATH_ADMIN + "/products-category", productCategoryRoutes);
+
+    app.use(PATH_ADMIN + "/roles", roleRoutes);
 }
