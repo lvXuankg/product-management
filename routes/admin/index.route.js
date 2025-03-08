c31d33bdbe74b55263a5419e4f19d789b38f7b5e
Le Viet Xuan <xuantryingbetter@gmail.com>
2025-03-09 02:59:38 +0700

    login

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -4,6 +4,7 @@ const productCategoryRoutes = require("./product-category.route");
 const systemConfig = require("../../config/system");
 const roleRoutes = require("./role.route");
 const accountRoutes = require("./account.route");
+const authRoutes = require("./auth.route");
 module.exports = (app) => {
     const PATH_ADMIN = systemConfig.prefixAdmin;
 
@@ -16,4 +17,6 @@ module.exports = (app) => {
     app.use(PATH_ADMIN + "/roles", roleRoutes);
 
     app.use(PATH_ADMIN + "/accounts", accountRoutes);
+
+    app.use(PATH_ADMIN + "/auth", authRoutes);
 }
