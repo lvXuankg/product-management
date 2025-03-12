08e5706f46d4a8f37a9c4fb89bb603fcd5674c88
Le Viet Xuan <xuantryingbetter@gmail.com>
2025-03-13 06:13:45 +0700

    category in client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,7 +1,10 @@
 const productRoutes = require("./product.route")
 const homeRoutes = require("./home.route")
 
+const categorymiddleware = require("../../middlewares/client/category.middleware");
+
 module.exports = (app) => {
+    app.use(categorymiddleware.category);
     app.use("/", homeRoutes);
     
     app.use("/products", productRoutes);
