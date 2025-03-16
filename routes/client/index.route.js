34876fef8d1321d5d55baf86c073b446ee941682
Le Viet Xuan <xuantryingbetter@gmail.com>
2025-03-16 20:57:54 +0700

    search page

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,11 +1,14 @@
 const productRoutes = require("./product.route")
 const homeRoutes = require("./home.route")
-
+const searchRoutes = require("./search.route");
 const categorymiddleware = require("../../middlewares/client/category.middleware");
 
 module.exports = (app) => {
     app.use(categorymiddleware.category);
+
     app.use("/", homeRoutes);
     
     app.use("/products", productRoutes);
+    
+    app.use("/search", searchRoutes);
 }
