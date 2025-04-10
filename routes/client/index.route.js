8fac424d900cd6ba29a59ce3536ffd21e8b0b296
Le Viet Xuan <xuantryingbetter@gmail.com>
2025-04-10 09:15:31 +0700

    register user

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -5,6 +5,8 @@ const cartRoutes = require("./cart.route");
 const categorymiddleware = require("../../middlewares/client/category.middleware");
 const cartMiddeware = require("../../middlewares/client/cart.middleware");
 const checkoutRoutes = require("./checkout.route");
+const userRoutes = require("./user.route");
+
 
 module.exports = (app) => {
     app.use(categorymiddleware.category);
@@ -20,4 +22,6 @@ module.exports = (app) => {
     app.use("/cart", cartRoutes);
 
     app.use("/checkout", checkoutRoutes);
+
+    app.use("/user", userRoutes);
 }
