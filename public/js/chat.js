ad68856b985ce350c3dae4b75d91db6a151530df
Le Viet Xuan <xuantryingbetter@gmail.com>
2025-04-15 21:36:46 +0700

    typing

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,6 +10,7 @@ if(formSendData){
         if(content){
             socket.emit("CLIENT_SEND_MESSAGE", content);
             e.target.elements.content.value = "";
+            socket.emit("CLIENT_SEND_TYPING", "hidden");
         }
     });
 }
@@ -19,6 +20,7 @@ if(formSendData){
 socket.on("SERVER_RETURN_MESSAGE", (data) => {
     const myId = document.querySelector("[my-id]").getAttribute("my-id");
     const body = document.querySelector(".chat .inner-body");
+    const boxTyping = document.querySelector(".inner-list-typing");
 
     let htmlFullName = "";
 
@@ -37,7 +39,7 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
         <div class="inner-content">${data.content}</div>
     `;
 
-    body.appendChild(div);
+    body.insertBefore(div, boxTyping);
 
     body.scrollTop = body.scrollHeight;
 });
@@ -49,6 +51,18 @@ if(bodyChat){
     bodyChat.scrollTop = bodyChat.scrollHeight;
 }
 // End Scroll Chat to Bottom 
+
+// showTyping 
+var timeOut;
+const showTyping = () => {
+    socket.emit("CLIENT_SEND_TYPING", "show");
+
+    clearTimeout(timeOut);
+    timeOut = setTimeout(()=>{
+        socket.emit("CLIENT_SEND_TYPING", "hidden");
+    }, 3000);
+}
+// End showTyping 
 
 // emoji-picker 
 // Show PopUp 
@@ -64,6 +78,7 @@ if(buttonIcon){
 
 
 // Insert Icon 
+
 const emojiPicker = document.querySelector('emoji-picker');
 if(emojiPicker){
     const inputChat = document.querySelector(".chat .inner-form input[name='content']");
@@ -71,6 +86,54 @@ if(emojiPicker){
     emojiPicker.addEventListener('emoji-click', event => {
         const icon = event.detail.unicode;
         inputChat.value = inputChat.value + icon;
+
+        const end = inputChat.value.length;
+        inputChat.setSelectionRange(end, end);
+        inputChat.focus();
+
+        showTyping();
     })
+
+    
+
+    inputChat.addEventListener("keyup", () => {
+        showTyping();
+    });
 }
 // End emoji-picker 
+
+// SERVER_RETURN_TYPING
+const elementListTyping = document.querySelector(".chat .inner-list-typing");
+if(elementListTyping){
+    socket.on("SERVER_RETURN_TYPING", (data) => {
+        if(data.type == "show"){
+            const existTyping = elementListTyping.querySelector(`[user-id="${data.userId}"]`);
+
+            if(!existTyping){
+                const boxTyping = document.createElement("div");
+                boxTyping.classList.add("box-typing");
+                boxTyping.setAttribute("user-id", data.userId);
+                boxTyping.innerHTML = `
+                    <div class="inner-name">${data.fullName}</div>
+                    <div class="inner-dots">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                `;
+    
+                elementListTyping.appendChild(boxTyping);
+                bodyChat.scrollTop = bodyChat.scrollHeight;
+            }
+        } else {
+            const boxTypingRemove = elementListTyping.querySelector(`[user-id="${data.userId}"]`);
+
+            if(boxTypingRemove){
+                elementListTyping.removeChild(boxTypingRemove);
+            }
+        }
+
+    });
+}
+
+// End SERVER_RETURN_TYPING
